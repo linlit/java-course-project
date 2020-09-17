03ba107c80b2789133ee635d92d57f35c6624126
DotsenkoMax <raxfaxmax@gmail.com>
2020-09-17 08:30:17 +0300

    Server+-

--- src/main/java/com/acme/edu/Server/Server.java
+++ src/main/java/com/acme/edu/Server/Server.java
@@ -1,5 +1,34 @@
 package com.acme.edu.Server;
 
-public class Server {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+/*final Socket clientConnection = connectionPortListener.accept();
+           final DataInputStream input = new DataInputStream(
+                   new BufferedInputStream(
+                           clientConnection.getInputStream()));
+           final DataOutputStream out = new DataOutputStream(
+                   new BufferedOutputStream(
+                           clientConnection.getOutputStream()))*/
 
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        try (final ServerSocket connectionPortListener = new ServerSocket(10_000);
+           ){
+            ExecutorService executor =  Executors.newFixedThreadPool(1000);
+            while(true){
+                final Socket clientConnection = connectionPortListener.accept();
+                executor.submit(() -> run(clientConnection));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void run(Socket clientConnection){
+
+    }
 }
+
---
+++ src/main/java/com/acme/edu/configs/Config.java
@@ -0,0 +1,6 @@
+package com.acme.edu.configs;
+
+public class Config {
+
+
+}
